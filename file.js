f3c9de9bb065bd2cbaf445f37d40cad3458a20e5
Kasper Paloc <kasperpaloc1@yahoo.com>
2018-11-07 10:47:49 -0500

    nice

---
+++ file.js
@@ -0,0 +1,12 @@
+function sayHello() {
+  console.log("Hello, World!");
+}
+
+function popHello() {
+  alert("Hello, World!");
+}
+
+function generateHello() {
+  p = document.getElementById ("hello");
+  p.innerhtml = "Hello, World!";
+}
